aa0e57533a77cecc3b007a036ff26d5d175ec6a7
Reece Mickelson <spartannumber2@gmail.com>
2021-12-12 03:08:38 -0700

    Update color

--- js/chart.js
+++ js/chart.js
@@ -20,7 +20,7 @@ function doGraph() {
             datasets: [{
             fill: false,
             pointRadius: 0,
-            borderColor: '#252525',
+            borderColor: '#ffffff',
             data: yValues
             }]
         },    
